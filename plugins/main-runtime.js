8c518e612a7b0b2209232fe88ffad831af9d107b
ɳoɓꀤʈɑ 👑🖤🎧 <164648341+ayazaliofc@users.noreply.github.com>
2024-12-19 12:55:19 +0500

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ m.react('⏳')
       }) * 1000
     }
     let muptime = clockString(_muptime)
-    let str = `🟢ᴘʀɪɴᴄᴇ ᴍᴅ ᴜᴘᴛɪᴍᴇ\n\n🎗️ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
+    let str = `🍁 ᴜᴘᴛɪᴍᴇ\n\n🎗️ʙᴏᴛ ᴀᴄᴛɪᴠᴇ ᴅᴜʀᴀᴛɪᴏɴ\n *${muptime}⏰*`
     conn.sendMessage(m.chat, {
       text: str,
       contextInfo: {
@@ -18,13 +18,13 @@ m.react('⏳')
       mentionedJid: [m.sender],
       isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363199257221654@newsletter',
+                newsletterJid: '120363174979265795@newsletter',
                 newsletterName: global.author,
                 serverMessageId: -1
             },
       forwardingScore: 999,
       externalAdReply: {
-      title: "🎗️ᴘʀɪɴᴄᴇ ᴍᴅ🎗️",
+      title: "ɳoɓꀤʈɑ 👑🖤🎧",
       body: "ʀᴜɴɪɴɢ sɪɴᴄᴇ",
       thumbnailUrl: pp,
       sourceUrl: '',
